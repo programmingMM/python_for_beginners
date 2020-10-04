96e927a5fdce08e2b3fd71383658754a9774085b
Min Lwin <minlwinkyaw307@gmail.com>
2020-10-04 17:45:30 +0300

    After

--- hello.py
+++ hello.py
@@ -1,8 +1,18 @@
+# name - "string"
+# age - 22, 24 (integer)
+# height - 5.4, 6.1 (float)
+# status - True, False (boolean)
+
+
 a = 11
-b = 2
-
-test = 12.3
+b = 3.142
 name = "Min Lwin"
-hello = "Hello, world!"
-
-print(b)
+single = True
+# int = integer
+print(type(a))
+# float
+print(type(b))
+# str = string
+print(type(name))
+# bool - boolean
+print(type(single))
